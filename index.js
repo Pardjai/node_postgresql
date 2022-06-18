4839ed9679aa6ae5145fe6161f7ca8a9186eb425
Pardjai <Ilia.Kozionov@yandex.ru>
2022-06-18 15:44:35 +0300

    moved routers-functional to index, deleted routers-folder

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const sequelize = require('./utils/database')
+const bcrypt = require('bcryptjs')
 const fileMiddleware = require('./middleware/file')
-const profilesRoutes = require('./routes/profiles')
-const profileRoutes = require('./routes/profile')
-const userRoutes = require('./routes/user')
+const User = require('./models/user')
+const {validationResult} = require('express-validator/check')
+const {profileEditValidators, registerValidators, loginValidators} = require('./utils/validators')
+const {CHANGEABLE_FIELDS} = require('./keys')
 
 const app = express()
 
@@ -12,9 +14,148 @@ app.use(express.json())
 app.use(fileMiddleware.single('photo_url'))
 app.use('/images',express.static(path.join(__dirname, 'images')))
 
-app.use('/profiles', profilesRoutes)
-app.use('/profile', profileRoutes)
-app.use('/user', userRoutes)
+// === ROUTES *START* ===
+
+//=== /profiles.  (GET) ===
+//
+app.get('/profiles',  async (req, res) => {
+  try {
+      const limit = 10
+      const page = +req.query.page
+      let offset = (page - 1) * limit 
+      if (Number.isNaN(offset)){
+          offset = 0
+      }
+      const users = await User.findAll({
+          order: [['createdAt']],
+          limit,
+          offset
+      })
+
+      res.status(200).json(users)
+  } catch (err) {
+      console.error(`Get users error: ${err}`);
+  }
+})
+
+//=== /profile. (GET, PUT) ===
+//
+app.get('/profile/:id', async (req, res) => {
+  try {
+
+      const user = await User.findByPk(+req.params.id)
+
+      res.status(200).json({user})
+      
+  } catch (err) {
+      console.error(`Get user error: ${err}`);
+  }
+})
+//======================================
+app.put('/profile/:id', profileEditValidators, async (req, res) => {
+  try {
+
+      const errors = validationResult(req);
+       if (!errors.isEmpty()) {
+          console.log(errors);
+          return res.status(422).json('Invalid profile-edit-data. Not processed');
+       }
+
+      const user = await User.findByPk(+req.params.id)
+
+      CHANGEABLE_FIELDS.forEach(fieldName => {
+          user[fieldName] = req.body[fieldName] ?? user[fieldName]
+      })
+
+      if (req.file) {
+          user.photo_url = req.file.path
+      }
+      
+      await user.save()
+      
+      res.status(200).json(user)
+  } catch (err) {
+      console.error(`Update user ${err}`);
+  }
+})
+
+//=== /user. (POST) ===
+//
+app.post('/user/register',  registerValidators, async (req, res) => {
+  try {
+
+      const errors = validationResult(req);
+       if (!errors.isEmpty()) {
+          console.log(errors);
+          return res.status(422).json('Invalid register-data. Not processed');
+       }
+
+      const {name, email, password} = req.body
+
+      const [candidate] = await User.findAll({
+          where:{
+              email
+          }
+      })
+
+      if (candidate){
+          throw new Error('Attempt to add an existing user')
+      }
+
+      const hashPassword = await bcrypt.hash(password, 12)
+      const user = await User.create({
+          name,
+          email,
+          password: hashPassword
+      })
+
+      res.status(201).json({user})
+
+  } catch (err) {
+      console.error(`Register ${err}`);
+      res.status(500).json({
+          message: 'Server error during registration'
+      })
+  }
+})
+//======================================
+app.post('/user/login',  loginValidators, async (req, res) => {
+  try {
+
+      const errors = validationResult(req);
+       if (!errors.isEmpty()) {
+          console.log(errors);
+          return res.status(422).json('Invalid login-data. Not processed');
+       }
+
+      const {email, password} = req.body
+      const [candidate] = await User.findAll({
+          where: {
+              email
+          }
+      })
+
+      if (! candidate){
+          throw new Error('Received unknown email')
+      }
+      const userPassword = candidate.password
+
+      const isValidPassword = await bcrypt.compare(password, userPassword)
+      if (! isValidPassword){
+          throw new Error('Received invalid password')
+      }
+
+      res.status(200).json('Authentication was successful')
+
+  } catch (err) {
+      console.error(`Login ${err}`);
+      res.status(500).json({
+          message: 'Server error during login'
+      })
+  }
+})
+
+// === *END* ROUTES ===
 
 const PORT = process.env.PORT || 3000
 async function start(){
